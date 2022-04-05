328d1212c0edb42fe43ddf2b4c9af5fcb34c0017
cazagajo <josuepoolchable@gmail.com>
2022-04-04 23:50:07 -0500

    Primer comit de archivo

---
+++ app/src/main/java/com/example/splash/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Hoome_Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(tarea, 1000);
+    }
+}
